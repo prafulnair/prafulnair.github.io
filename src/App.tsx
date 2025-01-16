0fea347eae65cfa59b4c94bdb8744cda43e0c8b5
Praful Nair <prafulworkspace@gmail.com>
2025-01-16 18:04:00 -0500

    Update2 App.tsx with tempalte landing page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 // function App() {
@@ -44,4 +44,4 @@ function App() {
 export default App;
 
 
-export default App
+
